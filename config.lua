26d081f857c1f86ab66ab699f07759ec9ea2122b
yunglean_ <71585779+yunglean4171@users.noreply.github.com>
2023-03-31 14:30:03 +0200

    v0.2.1

---
+++ config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+Config.zones = {
+    {x = 1847.916015625, y = 3675.8190917968, z = 33.767009735108},
+    {x = -1688.43811035156, y = -1073.62536621094, z = 13.1521873474121},
+    {x = -2195.1352539063, y = 4288.7290039063, z = 49.173923492432}
+}
+
+Config.notificationText = '⚠️ You have entered No-Flight Zone you will get shot down soon!⚠️'
+
+Config.displayZoneMarker = false
